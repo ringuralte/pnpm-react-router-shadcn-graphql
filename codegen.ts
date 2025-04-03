bb8ea3df17a5d28122af53ef7b33efa9237a97ad
ringuralte <ringuralte@gmail.com>
2025-04-03 13:00:18 +0530

    Project setup

---
+++ codegen.ts
@@ -0,0 +1,19 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:8002/arsi",
+  documents: ["app/**/*.tsx", "app/**/*.ts"],
+  generates: {
+    "app/gql/": {
+      preset: "client",
+      plugins: []
+    },
+    "./graphql.schema.json": {
+      plugins: ["introspection"]
+    }
+  }
+};
+
+export default config;
